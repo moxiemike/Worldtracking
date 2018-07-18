27ae2f8e28848be2fc0b1a77a3dfc31804cf381a
JosephS <JRySocarras@gmail.com>
2018-07-18 17:32:31 -0500

    Detecting Feature Points and Tracking Real-World Position and Orientation

---
+++ World Tracking/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  World Tracking
+//
+//  Created by Joseph on 7/18/18.
+//  Copyright © 2018 55B.ai. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ World TrackingTests/World_TrackingTests.swift
@@ -0,0 +1,34 @@
+//
+//  World_TrackingTests.swift
+//  World TrackingTests
+//
+//  Created by Joseph on 7/18/18.
+//  Copyright © 2018 55B.ai. All rights reserved.
+//
+
+import XCTest
+@testable import World_Tracking
+
+class World_TrackingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
